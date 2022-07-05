12ffaf731b72b51312d26d346b13d2893b2c7b80
Jann <janncarlomoreno@gmail.com>
2022-07-05 00:07:41 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+window.addEventListener("load", function () {
+  const date = new Date();
+  const tatianaBirthdate = new Date("07/05/1999");
+  console.log(tatianaBirthdate);
+  const actualYear = date.getFullYear();
+
+  let text = document.getElementById("letter-body-text").innerHTML;
+  text = text.replace("$year", actualYear.toString());
+  text = text.replace("$age", `${actualYear - tatianaBirthdate.getFullYear()}`);
+  document.getElementById("letter-body-text").innerHTML = text;
+  console.log(text);
+});
