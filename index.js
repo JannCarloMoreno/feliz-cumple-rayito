9ccd35045b02930efedc691b658252917b70a997
Jann <janncarlomoreno@gmail.com>
2022-07-05 00:28:17 -0500

    Add day text to letter

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const getDayText = (day) =>
+  ["Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"][
+    day
+  ];
 window.addEventListener("load", function () {
   const date = new Date();
   const tatianaBirthdate = new Date("07/05/1999");
@@ -5,8 +9,8 @@ window.addEventListener("load", function () {
   const actualYear = date.getFullYear();
 
   let text = document.getElementById("letter-body-text").innerHTML;
+  text = text.replace("$day", getDayText(date.getDay()));
   text = text.replace("$year", actualYear.toString());
   text = text.replace("$age", `${actualYear - tatianaBirthdate.getFullYear()}`);
   document.getElementById("letter-body-text").innerHTML = text;
-  console.log(text);
 });
